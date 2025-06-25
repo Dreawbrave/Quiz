83a6e884d182f975d84c9d1c1434ea26df2fe5eb
Dreawbrave <ggecwa@gmail.com>
2025-06-25 13:32:48 +0700

    проект без fsd

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home'; // Создайте эту папку и файл!
+import About from './pages/About';
+
+export default function App() {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+            </Routes>
+        </BrowserRouter>
+    );
+}
